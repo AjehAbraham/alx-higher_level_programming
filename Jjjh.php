767216136be895cece70838b5c70dcfd5e45502f
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2023-06-02 15:14:32 +0100

    Update Jjjh.php

--- Jjjh.php
+++ Jjjh.php
@@ -1,7 +1,7 @@
 
 <?php
 
-echo "git clone https://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@github.com/AjehAbraham/alx-higher_level_programming.git"
+echo "git clone https://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@github.com/AjehAbraham/alx-higher_level_programming.git"
 
 
 
