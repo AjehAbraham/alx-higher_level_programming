943a045484a56abdb47b0360e605ab0cf15200e8
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2023-06-06 16:26:56 +0100

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- Update second_table Where name is Bob
 -- Change Bob socre to 10 without using the id of Bob
-UPDATE 'second_table' SET score = 10; WHERE 'name'= Bob;
+UPDATE `second_table` SET score= 10; WHERE `name`= "Bob";
