b95ab650a3dfc5d0f1c1aa3216e3e78f5022b5da
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2023-06-06 15:43:38 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- Creating table and inserting dats into tge Table without using SHOW and SELECT keyword
-CREATE TABLE IF NOT EXISTS second_table(`id`INT,`name` VARCHAR(256),`score` INT);
-INSERT INTO `second_table`(`id`,`name`,`score`) VALUES(id = 1, name = “John”, score = 10);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES(id= 2, name= “Alex”, score= 3);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES(id = 3, name = “Bob”, score = 14);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES(id = 4, name = “George”, score = 8);
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
