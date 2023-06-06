55c8343688f08edd74970fad758da51a986d7b5d
AjehAbraham <110047789+AjehAbraham@users.noreply.github.com>
2023-06-06 16:04:05 +0100

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 --List all the name and score,starting with the highest to the lowest score in Mysql Server
 -- Records are in descending order
-SELECT `score`,`name` 
+SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
